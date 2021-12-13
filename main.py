e15bb19b72016ea379450dda754ae0cfda2fc954
silthecodeman <68767768+silthecodeman@users.noreply.github.com>
2021-12-13 15:33:46 -0600

    dots program

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
+import PIL
+from PIL import Image
+
+
 if __name__ == "__main__":
-  print('hello world')
+  img = Image.open('Images/homer.jpg').convert('L')
+  width = img.size[0] 
+  height = img.size[1]
+  for i in range(0,width):
+        for j in range(0,height):
+            data = img.getpixel((i,j))
+            if data < 50:
+                img.putpixel((i,j),(0))
+            if data >= 50:
+                img.putpixel((i,j),(255))
+  img.save('dots.jpg')
